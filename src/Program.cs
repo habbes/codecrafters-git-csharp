c5c6e45b066300d459272f070555f262a63b6fc7
Clément Habinshuti <haby_habbes@live.com>
2025-09-08 19:49:57 +0300

    Naive implementation of git-cat-file

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.IO.Compression;
 
 if (args.Length < 1)
 {
@@ -16,6 +17,34 @@ if (command == "init")
     Directory.CreateDirectory(".git/refs");
     File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
     Console.WriteLine("Initialized git directory");
+}
+else if (command == "git-cat-file -p" && args.Length == 3 && args[0] == "-p")
+{
+    // See: https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
+    string objectHash = args[2];
+    string dirName = objectHash[..2];
+    string fileName = objectHash[2..];
+    string objectPath = Path.Combine(".git", "objects", dirName, fileName);
+    using var fileStream = File.OpenRead(objectPath);
+    using var zlibStream = new ZLibStream(fileStream, CompressionMode.Decompress);
+
+    using var reader = new StreamReader(zlibStream);
+    string content = reader.ReadToEnd();
+    // find null byte, which separates header from content
+    // header contains object type and size followed by null byte
+    int nullByteIndex = content.IndexOf('\0');
+    string header = content[..nullByteIndex];
+
+    string[] headerContent = header.Split(' ');
+    string type = headerContent[0];
+    if (type != "blob")
+    {
+        throw new ArgumentException($"Unsupported object type {type}");
+    }
+
+    string body = content[(nullByteIndex + 1)..];
+
+    Console.Write(body);
 }
 else
 {
